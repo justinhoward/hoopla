2ed8bb93012f4add5cae737755bc566198a08631
Justin Howard <jmhoward0@gmail.com>
2014-12-13 18:16:07 -0800

    added docblocks

--- src/Event.js
+++ src/Event.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ * A named event
+ *
+ * @param {string} name The name of the event
+ * @param {object} [attributes] Custom attributes for the event
+ */
 function Event(name, attributes) {
     this._name = name;
     this._stopped = false;
@@ -11,23 +17,52 @@ function Event(name, attributes) {
 }
 var proto = Event.prototype;
 
+/**
+ * Get the event name
+ *
+ * @return {string} The event name
+ */
 proto.getName = function() {
     return this._name;
 };
 
+/**
+ * Get an event attribute
+ *
+ * @param {string} name The attribute name
+ * @return {mixed} The attribue value
+ */
 proto.get = function(name) {
     return this._attrs[name];
 };
 
+/**
+ * Set an event attribute
+ *
+ * @param {string} name The attribute name
+ * @param {mixed} value The attribute value
+ * @return {this}
+ */
 proto.set = function(name, value) {
     this._attrs[name] = value;
     return this;
 };
 
+/**
+ * Get an object containing all the event attributes
+ *
+ * @return {object} An object whose keys are attribute names and values are their values.
+ */
 proto.getAttributes = function() {
     return this._attrs;
 };
 
+/**
+ * Set event attributes from an object whose keys are attribute names.
+ * The values will be merged with existing attributes.
+ *
+ * @param {object} attributes The attributes to set
+ */
 proto.setAttributes = function(attributes) {
     for (var key in attributes) {
         if (attributes.hasOwnProperty(key)) {
@@ -36,15 +71,32 @@ proto.setAttributes = function(attributes) {
     }
 };
 
+/**
+ * Check if the given attribute is set
+ *
+ * @param {string} name The attribute name
+ * @return {boolean} True if the attribute is set, false if not
+ */
 proto.has = function(name) {
     return this._attrs.hasOwnProperty(name);
 };
 
+/**
+ * Stop propagation for this event. Additional listeners will
+ * not be called.
+ *
+ * @return {this}
+ */
 proto.stopPropagation = function() {
     this._stopped = true;
     return this;
 };
 
+/**
+ * Check if propagation is stopped
+ *
+ * @return {boolean} True if propagation is stopped, false if not.
+ */
 proto.isPropagationStopped = function() {
     return this._stopped;
 };
--- src/Hoopla.js
+++ src/Hoopla.js
@@ -2,6 +2,9 @@
 
 var Event = require('./Event');
 
+/**
+ * The Hoopla event dispatcher
+ */
 function Hoopla() {
     this._listeners = {};
     this._negativeListeners = {};
@@ -9,6 +12,13 @@ function Hoopla() {
 }
 var proto = Hoopla.prototype;
 
+/**
+ * Dispatch an event and call its listeners in order of priority.
+ *
+ * @param {Event|string} event The Event object to dispatch, or the event name
+ * @param {object} [attributes] If passing an event name, the attributes for the event
+ * @return {this}
+ */
 proto.dispatch = function(event) {
     if (typeof event === 'string') {
         event = this.createEvent(event, arguments[1]);
@@ -29,6 +39,15 @@ proto.dispatch = function(event) {
     }
 };
 
+/**
+ * Add a listener for `eventName`
+ *
+ * @param {string} eventName The name of the event to listen to
+ * @param {function} listener The function to be called when the event is dispatched
+ * @param {integer} priority The priority of the listener where lower numbers are
+ *     called first. Negatives are allowed.
+ * @return {this}
+ */
 proto.addListener = function(eventName, listener, priority) {
     var listeners;
     if (typeof priority === 'undefined') {
@@ -54,6 +73,12 @@ proto.addListener = function(eventName, listener, priority) {
     return this;
 };
 
+/**
+ * Get the listener functions for `eventName` in order of priority
+ *
+ * @param {string} eventName The name of the event to get listeners for
+ * @return {array[function]} An array of listeners
+ */
 proto.getListeners = function(eventName) {
     if (this._sorted[eventName]) {
         return this._sorted[eventName];
@@ -72,12 +97,25 @@ proto.getListeners = function(eventName) {
     return sorted;
 };
 
+/**
+ * Check whether the given `eventName` has any listeners
+ *
+ * @param {string} eventName The name of the event to check
+ * @return {boolean} True if the event has listeners, false if not
+ */
 proto.hasListeners = function(eventName) {
     return !!this.getListeners(eventName).length;
 };
 
-proto.createEvent = function(name, attributes) {
-    return new Event(name, attributes);
+/**
+ * Create an Event object
+ *
+ * @param {string} eventName The name of the event
+ * @param {object} [attributes] Attributes to set on the Event
+ * @return {Event} The created Event
+ */
+proto.createEvent = function(eventName, attributes) {
+    return new Event(eventName, attributes);
 };
 
 module.exports = Hoopla;
